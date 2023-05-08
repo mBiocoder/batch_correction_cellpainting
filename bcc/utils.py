cc76ebaf50dee649a7a610a496bf55ccc599abf5
Sonja-Stockhaus <105167760+Sonja-Stockhaus@users.noreply.github.com>
2023-05-08 20:56:36 +0200

    Feat/scrape sirna targets (#22)
    Authored-by: Sonja Stockhaus <stockhaus@cip.ifi.lmu.de>

--- bcc/utils.py
+++ bcc/utils.py
@@ -1,7 +1,60 @@
 import scarches
+import pandas as pd
+import urllib.request
+from bs4 import BeautifulSoup
+from typing import Literal
 
 
 def get_scarches_version():
     """Return the version of the SCARCHES package."""
 
     return scarches.__version__
+
+
+def get_sirna_target_gene(
+    sirna: str, return_as: Literal["gene_symbol", "entrez"] = "gene_symbol"
+) -> str:
+    """Function to get the target gene(s) of a siRNA given its ThermoFisher ID
+
+    Given the ThermoFisher ID of a siRNA, its target gene(s) are fetched from the
+    ThermoFisher website.
+    If a siRNA has multiple target genes, the returned string consists of all targets
+    separated by an underscore "_".
+    If no targets can be found for a siRNA, pd.NA is returned.
+
+    Args:
+        sirna:      ThermoFisher ID of a siRNA
+        return_as:  determines whether the gene symbol (gene_symbol) or ENTREZ
+                    gene ID (entrez) of the target(s) are returned
+
+    Returns:
+        str:    target gene(s) of the given siRNA, retrieved from ThermoFisher website
+
+    """
+
+    page = urllib.request.urlopen(
+        "https://www.thermofisher.com/order/genome-database/AssayDetails?rev=V302&"
+        f"assayid={sirna}&assaytype=sirna&CID=&ICID="
+    )
+    soup = BeautifulSoup(page, "html.parser")
+    results = soup.find_all("dl")
+
+    if len(results) == 0:
+        # retrun NA if no target gene was found
+        return pd.NA
+
+    genes = []
+    if return_as == "gene_symbol":
+        for r in results:
+            gene = str(r).split("<dt>Gene Symbol</dt>\n<dd>")[1].split("</dd>")[0]
+            genes.append(gene)
+    else:
+        for r in results:
+            gene = (
+                str(r)
+                .split("<dt>Entrez Gene ID</dt>\n<dd><")[1]
+                .split(">")[1]
+                .split("<")[0]
+            )
+            genes.append(gene)
+    return "_".join(genes)
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,15 +1,37 @@
 import unittest
 
 import bcc
+import pandas as pd
+
 
 class utils_TestClass(unittest.TestCase):
     def setUp(self):
-        
         self.scarches_version = bcc.utils.get_scarches_version()
-        
+
     def test_scarches_over_0_1_0(self):
-        
         major, minor, revision = [int(x) for x in self.scarches_version.split(".")]
-        
 
         assert all([major >= 0, minor >= 1, revision >= 0])
+
+    def test_sirna_target_webscraping(self):
+        # this siRNA has no target annotated (as of 08.05.2023)
+        x = bcc.utils.get_sirna_target_gene("s21506")
+        assert pd.isnull(x)
+        x = bcc.utils.get_sirna_target_gene("s21506", "entrez")
+        assert pd.isnull(x)
+        x = bcc.utils.get_sirna_target_gene("nonsense")
+        assert pd.isnull(x)
+        x = bcc.utils.get_sirna_target_gene("nonsense", "entrez")
+        assert pd.isnull(x)
+
+        # test gene name for one or multiple targets
+        x = bcc.utils.get_sirna_target_gene("s21480")
+        assert x == "SUB1"
+        x = bcc.utils.get_sirna_target_gene("s21422")
+        assert x == "DHRS4L2_DHRS4"
+
+        # test entrez ID for one or multiple targets
+        x = bcc.utils.get_sirna_target_gene("s21480", "entrez")
+        assert x == "10923"
+        x = bcc.utils.get_sirna_target_gene("s21422", "entrez")
+        assert x == "317749_10901"
