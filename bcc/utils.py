120b390d4a1bede9f981d2ef3c7aefc02ac6742a
Mahima Arunkumar <mahima.arunkumar.1@gmail.com>
2023-05-18 22:06:32 +0200

    Add function to utils and import from utils

--- bcc/utils.py
+++ bcc/utils.py
@@ -4,6 +4,14 @@ import urllib.request
 from bs4 import BeautifulSoup
 from typing import Literal
 
+import matplotlib.pyplot as plt
+import numpy as np
+from typing import Optional
+import sys
+import os
+sys.path.append('rxrx1-utils')
+import rxrx.io as rio
+
 
 def get_scarches_version():
     """Return the version of the SCARCHES package."""
@@ -58,3 +66,68 @@ def get_sirna_target_gene(
             )
             genes.append(gene)
     return "_".join(genes)
+
+
+def visualize(data: str, experiment: str, plate: int, well: str, site: int, channels: Optional[list] = [1, 2, 3, 4, 5, 6], ax=None) -> np.ndarray:
+    """Visualize the respective rxrx1 image given data, experiment, plate, well, and site information using the loa_site() function provided by Recursion.
+
+    Args:
+        data: "train" or "test" set.
+        experiment: Experiment identifier.
+        plate: Plate number.
+        well: Well identifier.
+        site: Site number.
+        channels: List of channel numbers to visualize (default: [1, 2, 3, 4, 5, 6]).
+        ax: Optional matplotlib Axes object to plot the image (default: None).
+
+    Returns:
+        The RGB image as a NumPy array.
+    """
+
+    img_tensor = rio.load_site(data, experiment, plate, well, site, channels)
+    img = rio.convert_tensor_to_rgb(img_tensor)
+    
+    if ax is None:
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+    ax.imshow(img)
+    ax.set_axis_off()
+    return img
+
+
+
+def visualize_cells_with_sirna(data: str, experiment: str, well: str, site: int, sirna: str, max_plate_number: int, plate: Optional[int] = None, channels: list = [1, 2, 3, 4, 5, 6]):
+    """
+    Visualizes cells with a specific siRNA treatment across different plates.
+    
+    Args:
+        data: "train" or "test" set.
+        experiment: Name of the experiment.
+        well: Well identifier.
+        site: Site identifier.
+        sirna: siRNA treatment.
+        max_plate_number: Maximum number of plates for this dataset.
+        plate: Plate number. If None, visualize all plates.
+        channels: List of channel numbers to visualize.
+
+    Returns:
+        None
+    """
+    well_size = 10
+    title_fs = 25
+
+    fig = plt.figure(figsize=(5 * well_size, 3 * well_size))
+
+    if plate is None:
+        for plate in range(1,  max_plate_number + 1):
+            ax = fig.add_subplot(2, 4, plate)
+            visualize(data, experiment, plate, well, site=site, channels=channels, ax=ax)
+            ax.set_title(f'{experiment} Plate {plate}, siRNA {sirna}, well {well}, site {site}', fontdict={'fontsize': title_fs})
+
+    else:
+        y = rio.load_site_as_rgb(data, experiment, plate, well, site)
+        plt.figure(figsize=(3, 3))
+        plt.axis('off')
+        plt.title(f'{experiment} Plate {plate}, siRNA {sirna}, well {well}, site {site}', fontdict={'fontsize': 7})
+        _ = plt.imshow(y)
+
