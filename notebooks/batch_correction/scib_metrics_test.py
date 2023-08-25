e2dd9d7affca29dc2903d72983e0c274b2c46ba5
Sonja Stockhaus <stockhaus@cip.ifi.lmu.de>
2023-08-25 17:14:36 +0200

    update metrics test file

--- notebooks/batch_correction/scib_metrics_test.py
+++ notebooks/batch_correction/scib_metrics_test.py
@@ -1,5 +1,5 @@
 import scanpy as sc
-from bcc.scib_metrics import evaluate
+from bcc.scib_metrics import evaluate, metrics_preparation
 
 
 adata_unintegrated = sc.read_h5ad(
@@ -11,10 +11,12 @@ adata_integrated = sc.read_h5ad(f"batch_correction_cellpainting/data/jump/scanvi
 adata_unintegrated = adata_unintegrated[
     adata_unintegrated.obs.Metadata_Source == "source_9"
 ].copy()
+adata_unintegrated = metrics_preparation(adata_unintegrated, "Metadata_JCP2022")
 
 adata_integrated = adata_integrated[
     adata_integrated.obs.Metadata_Source == "source_9"
 ].copy()
+adata_integrated = metrics_preparation(adata_integrated, "Metadata_JCP2022", True)
 
 df = evaluate(
     adata_unintegrated,
